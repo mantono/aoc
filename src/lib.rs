a47b73bacfe3e4c7c360ec052b7b4c5355d99cf5
Anton Österberg <anton.osterberg@gmail.com>
2021-12-04 19:19:45 +0100

    Allow inferring puzzle from current date

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ mod input;
 mod puzzle;
 
 pub use crate::input::get_input;
-pub use crate::puzzle::{Part, Puzzle};
+pub use crate::puzzle::Puzzle;
--- src/puzzle.rs
+++ src/puzzle.rs
@@ -1,3 +1,5 @@
+use chrono::{Datelike, Utc};
+
 #[derive(Debug, Clone, Copy)]
 pub struct Puzzle {
     pub(crate) year: u16,
@@ -7,6 +9,22 @@ pub struct Puzzle {
 impl Puzzle {
     pub fn new(year: u16, day: u8) -> Puzzle {
         Puzzle { year, day }
+    }
+
+    pub fn today() -> Puzzle {
+        // Puzzles unlock at midnight EST/UTC-5
+        let offset = chrono::FixedOffset::west(5 * 3600);
+        let date = Utc::now().with_timezone(&offset);
+        Puzzle {
+            year: date.year() as u16,
+            day: date.day() as u8,
+        }
+    }
+}
+
+impl Default for Puzzle {
+    fn default() -> Self {
+        Self::today()
     }
 }
 
