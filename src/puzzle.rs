de4bf599c15b26e9abfed7981823d00d6b3b5e26
Anton Österberg <anton.osterberg@gmail.com>
2021-12-04 19:18:45 +0100

    Remove "Part" from Puzzle

--- src/puzzle.rs
+++ src/puzzle.rs
@@ -2,27 +2,16 @@
 pub struct Puzzle {
     pub(crate) year: u16,
     pub(crate) day: u8,
-    pub(crate) part: Part,
 }
 
 impl Puzzle {
-    pub fn new(year: u16, day: u8, part: Part) -> Puzzle {
-        Puzzle { year, day, part }
+    pub fn new(year: u16, day: u8) -> Puzzle {
+        Puzzle { year, day }
     }
 }
 
 impl std::fmt::Display for Puzzle {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let part: &str = match self.part {
-            Part::One => "one",
-            Part::Two => "two",
-        };
-        write!(f, "{} day {} part {}", self.year, self.day, part)
+        write!(f, "{}-{}", self.year, self.day)
     }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum Part {
-    One,
-    Two,
 }
