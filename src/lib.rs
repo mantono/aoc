87bc4d01544674958ff895b706b635bab02f1e6b
Anton Österberg <anton.osterberg@gmail.com>
2021-12-06 19:55:48 +0100

    Doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@ pub use crate::solve::Solver;
 
 /// Take an implementation of Solver and use it to parse
 /// the puzzle input and solve the puzzle
-///
 pub fn run<T: Solver>() -> Result<String, String> {
     let puzzle: Puzzle = T::puzzle();
     let input: String = get_input(puzzle)?;
--- src/solve.rs
+++ src/solve.rs
@@ -1,12 +1,19 @@
 use crate::Puzzle;
 
+/// Solver trait with some default implementations of functions to reduce the amount
+/// of boiler plate needed to parse and process any puzzle input.
 pub trait Solver {
     type Item;
 
+    /// Returns which puzzle (year and day) the Solver is for. Will default to the
+    /// current day if no explicit implementation is done.
     fn puzzle() -> Puzzle {
         Puzzle::today()
     }
 
+    /// Take the whole input file and return an answer. Will try to solve for part
+    /// two first if a solution has been implemented for the second part, else it
+    /// will use the solution for part one.
     fn solve(input: String) -> String {
         let inputs: Vec<Self::Item> = Self::parse_input(input);
         let solution2: String = Self::solve_two(&inputs);
